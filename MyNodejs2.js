f739c909c60c6eb5063757879ddb26b67e78d594
JulyMao <1459291064@qq.com>
2018-06-29 11:25:18 +0800

    MyFirstNodejs

---
+++ MyNodejs2.js
@@ -0,0 +1,34 @@
+/*var fs = require('fs');
+
+var iconv = require('iconv-lite'); 
+fs.readFile('E:/node.js/node/input.txt',function(err,data){
+  if(err){
+  return console.error(err);
+}
+console.log('文件内容是：' + iconv.decode(data,'gbk'));
+});*/
+
+
+
+var fs = require('fs');
+var buf = new Buffer(1024);
+var iconv = require('iconv-lite');
+buf.e
+fs.open('E:/node.js/node/input.txt','r+',function(err,fd){
+  if(err)
+    return console.error(err);
+  console.log("打开文件成功，准备读取文件！")
+
+
+  fs.read(fd,buf,0,buf.length,0,function(err,bytes){
+    if(err)
+      return console.error(err);
+    console.log(iconv.decode(buf.slice(0,bytes),'gbk'));
+
+
+    fs.close(fd,function(err){
+      if(err)
+      return console.error(err);
+      });
+    });
+  });
---
+++ MyNodejs3.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var math = Math.random();
+console.log(math);
+fs.writeFile('E:/node.js/node/input2.txt','h嘿嘿嘿',function(err){
+  if(err)
+  return console.error(err);
+  console.log("写入成功！");
+});
