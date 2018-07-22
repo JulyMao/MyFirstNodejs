0e4afe33805ac7a6747a0f1b84aa29ab0be7c4bf
zzes <kuanglong0312@sina.com>
2018-07-23 07:33:39 +0800

    delete

--- MyNodejs1.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.createServer(function(request,response){
-  response.end('Hello World!');
-}).listen(3000);
-
-console.log('Server running at http://127.0.0.1:3000/');
--- MyNodejs3.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let math = Math.random();
-console.log(math);
-fs.writeFile('E:/node.js/node/input2.txt','h嘿嘿嘿',function(err){
-  if(err)
-  return console.error(err);
-  console.log("写入成功！");
-});
